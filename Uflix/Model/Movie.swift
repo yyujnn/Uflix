3297ed60a6578641155031ebb5f300a82225fa02
정유진 <yujin3504@gmail.com>
2025-03-23 22:04:10 +0900

    add: Model 구현

---
+++ Uflix/Model/Movie.swift
@@ -0,0 +1,23 @@
+//
+//  Movie.swift
+//  Uflix
+//
+//  Created by 정유진 on 3/23/25.
+//
+
+import Foundation
+
+struct MovieResponse: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
+    let id: Int?
+    let title: String?
+    let posterPath: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title
+        case posterPath = "poster_path"
+    }
+}
---
+++ Uflix/Model/Video.swift
@@ -0,0 +1,18 @@
+//
+//  Video.swift
+//  Uflix
+//
+//  Created by 정유진 on 3/23/25.
+//
+
+import Foundation
+
+struct VideoResponse: Codable {
+    let results: [Video]
+}
+
+struct Video: Codable {
+    let key: String
+    let site: String
+    let type: String
+}
