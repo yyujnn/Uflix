6054ae582bd9d086e3dbfdecdd732a0bde8cc137
정유진 <yujin3504@gmail.com>
2025-05-12 23:24:37 +0900

    feat: PosterCell 구현

--- Uflix/View/PosterCell.swift
+++ Uflix/View/PosterCell.swift
@@ -8,5 +8,45 @@
 import UIKit
 
 class PosterCell: UICollectionViewCell {
+    static let id = "PosterCell"
     
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .darkGray
+        imageView.layer.cornerRadius = 10
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        imageView.frame = contentView.bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        imageView.image = nil
+    }
+    
+    func configure(with movie: Movie) {
+        guard let posterPath = movie.posterPath else { return }
+        let urlString = "https://image.tmdb.org/t/p/w500/\(posterPath).jpg"
+        guard let url = URL(string: urlString) else { return }
+        
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.sync {
+                        self?.imageView.image = image
+                    }
+                }
+            }
+        }
+    }
 }
+
