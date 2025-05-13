6672b4e14b789cbcb60b7823c83a65949b935f05
정유진 <yujin3504@gmail.com>
2025-05-13 15:14:01 +0900

    feat: MainView configureUI

--- Uflix/View/MainViewController.swift
+++ Uflix/View/MainViewController.swift
@@ -6,14 +6,87 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainViewController : UIViewController {
 
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "NETFLIX"
+        label.textColor = UIColor(red: 229/255, green: 9/255, blue: 20/255, alpha: 1.0)
+        label.font = UIFont.systemFont(ofSize: 28, weight: .heavy)
+        return label
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionView.register(PosterCell.self, forCellWithReuseIdentifier: PosterCell.id)
+        collectionView.register(SectionHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SectionHeaderView.id)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .black
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    private func createLayout() -> UICollectionViewLayout {
+        return UICollectionViewLayout()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .black
+        [
+            label,
+            collectionView
+        ].forEach { view.addSubview($0) }
+        
+        label.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(10)
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).inset(10)
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(label.snp.bottom).offset(20)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide.snp.horizontalEdges)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
 
+}
+
+enum Section: Int, CaseIterable {
+    case popularMovies
+    case topRateMovies
+    case upcomingMovies
+    
+    var title: String {
+        switch self {
+        case .popularMovies: return "이 시간 핫한 영화"
+        case .topRateMovies: return "가장 평점이 높은 영화"
+        case .upcomingMovies: return "곧 개봉되는 영화"
+        }
+    }
+}
+
+extension MainViewController: UICollectionViewDelegate {
+    
+}
+
+extension MainViewController: UICollectionViewDataSource {
+    
+    // indexPath 별로 cell 을 구현
+    // tableView 의 cellForRowAt 과 비슷한 역할
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
 }
 
