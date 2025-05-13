e025f2de2020c50c97e6b10ad53fa675f1e5c4f4
정유진 <yujin3504@gmail.com>
2025-05-13 15:13:19 +0900

    feat: SectionHeaderView 구현

--- Uflix/View/SectionHeaderView.swift
+++ Uflix/View/SectionHeaderView.swift
@@ -6,7 +6,38 @@
 //
 
 import UIKit
+import SnapKit
 
 class SectionHeaderView: UICollectionReusableView {
+    static let id = "SectionHeaderView"
+
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.textColor = .white
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupUI() {
+        addSubview(titleLabel)
         
+        titleLabel.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().offset(20)
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+    func configure(with title: String) {
+        
+    }
+    
 }
