7a74a32637c8bbe9dca62f095cb0b66b1d49b4dc
정유진 <yujin3504@gmail.com>
2025-05-14 16:11:34 +0900

    feat: NetworkManager fetchData

--- Uflix/Common/NetworkManager.swift
+++ Uflix/Common/NetworkManager.swift
@@ -6,7 +6,48 @@
 //
 
 import Foundation
+import RxSwift
 
+enum NetworkError: Error {
+    case invalidUrl
+    case dataFetchFail
+    case decodingFail
+}
+
+// Singleton
 class NetworkManager {
+    
+    static let shared = NetworkManager()
+    private init() {}
+    
+    func fetch<T: Decodable>(url: URL) -> Single<T> {
+        return Single.create { observer in
+            let session = URLSession(configuration: .default)
+            session.dataTask(with: URLRequest(url: url)) { data, response, error in
+                
+                if let error = error {
+                    observer(.failure(error))
+                    return
+                }
+                
+                guard let data = data,
+                      let response = response as? HTTPURLResponse,
+                      (200..<300).contains(response.statusCode) else {
+                    observer(.failure(NetworkError.dataFetchFail))
+                    return
+                }
+                
+                do {
+                    let decodedData = try JSONDecoder().decode(T.self, from: data)
+                    observer(.success(decodedData))
+                } catch {
+                    observer(.failure(NetworkError.decodingFail))
+                }
+            }.resume()
+                
+            return Disposables.create()
+        }
+        
+    }
     
 }
