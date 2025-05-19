9b9cd01c4a5b898c98c003d1097ffd969fbd47df
정유진 <yujin3504@gmail.com>
2025-05-19 17:20:48 +0900

    feat: DetailVC Binding

--- Uflix/View/DetailViewController.swift
+++ Uflix/View/DetailViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 import SnapKit
 import YouTubeiOSPlayerHelper
 import RxSwift
+import Kingfisher
 
 class DetailViewController: UIViewController {
     private let viewModel: DetailViewModel
+    private let disposeBag = DisposeBag()
     
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+    private let stackView = UIStackView()
     private let posterImageView = UIImageView()
     private let titleLabel = UILabel()
     private let overviewLabel = UILabel()
@@ -30,63 +35,85 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureUI()
-        configureData()
+        setupUI()
         bind()
     }
     
     private func bind() {
+        viewModel.movieDetail
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] movie in
+                print(movie)
+                self?.configure(movie: movie)
+            }).disposed(by: disposeBag)
         
+        viewModel.trailerKey
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] key in
+                self?.playerView.load(withVideoId: key)
+            }).disposed(by: disposeBag)
+        
+        viewModel.error
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { error in
+                print("error: \(error.localizedDescription)")
+            }).disposed(by: disposeBag)
     }
     
-    private func configureUI() {
+    private func setupUI() {
         view.backgroundColor = .black
         
-        [
-            posterImageView,
-            titleLabel,
-            overviewLabel,
-            playerView,
-            likeButton
-        ].forEach { view.addSubview($0) }
+        // 스크롤뷰 설정
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints { $0.edges.equalToSuperview() }
         
-        posterImageView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(16)
-            $0.centerX.equalToSuperview()
-            $0.width.height.equalTo(150)
+        scrollView.addSubview(contentView)
+        
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalTo(scrollView.snp.width)
         }
         
-        titleLabel.snp.makeConstraints {
-            $0.top.equalTo(posterImageView.snp.bottom).offset(16)
+        contentView.addSubview(stackView)
+        
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        
+        stackView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(20)
             $0.left.right.equalToSuperview().inset(20)
         }
         
-        overviewLabel.snp.makeConstraints {
-            $0.top.equalTo(titleLabel.snp.bottom).offset(8)
-            $0.left.right.equalToSuperview().inset(20)
-        }
+        // 스타일 지정 (생략된 부분 다시 추가)
+        posterImageView.contentMode = .scaleAspectFit
+        posterImageView.layer.cornerRadius = 8
+        posterImageView.clipsToBounds = true
         
-        playerView.snp.makeConstraints {
-            $0.top.equalTo(overviewLabel.snp.bottom).offset(16)
-            $0.left.right.equalToSuperview().inset(20)
-            $0.height.equalTo(200)
-        }
-        
-        likeButton.snp.makeConstraints {
-            $0.top.equalTo(playerView.snp.bottom).offset(20)
-            $0.centerX.equalToSuperview()
-            $0.height.equalTo(44)
-        }
-        
-        titleLabel.font = .boldSystemFont(ofSize: 20)
-        overviewLabel.numberOfLines = 0
         likeButton.setTitle("❤️ 찜하기", for: .normal)
+        likeButton.setTitleColor(.white, for: .normal)
+        likeButton.backgroundColor = .darkGray
+        likeButton.layer.cornerRadius = 8
+        likeButton.snp.makeConstraints { $0.height.equalTo(44) }
+        
+        // stackView에 추가
+        [posterImageView, titleLabel, overviewLabel, playerView, likeButton].forEach {
+            stackView.addArrangedSubview($0)
+        }
+        
+        // playerView 고정 높이
+        playerView.snp.makeConstraints { $0.height.equalTo(200) }
     }
+
     
-    private func configureData() {
-        let movie = viewModel.movie
+    private func configure(movie: Movie) {
         titleLabel.text = movie.title
         overviewLabel.text = movie.overview
-        /// TODO: SDWebImage
+        
+        if let posterPath = movie.posterPath {
+            let url = URL(string: "https://image.tmdb.org/t/p/w500\(posterPath)")
+            posterImageView.kf.setImage(with: url)
+        }
     }
 }
