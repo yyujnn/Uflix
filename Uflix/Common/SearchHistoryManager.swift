afc63370784b755eb998878d3f44f9c015305924
정유진 <yujin3504@gmail.com>
2025-05-26 16:13:20 +0900

    feat: UserDefaults 검색어 저장 구현

--- Uflix/Common/SearchHistoryManager.swift
+++ Uflix/Common/SearchHistoryManager.swift
@@ -8,5 +8,21 @@
 import Foundation
 
 struct SearchHistoryManager {
-   
+    static let key = "searchHistory"
+    
+    static func load() -> [String] {
+        return UserDefaults.standard.stringArray(forKey: key) ?? []
+    }
+    
+    static func save(_ keyword: String) {
+        var history = load()
+        history.removeAll {$0 == keyword}
+        history.insert(keyword, at:0)
+        history = Array(history.prefix(10))
+        UserDefaults.standard.set(history, forKey: key)
+    }
+    
+    static func clear() {
+        UserDefaults.standard.removeObject(forKey: key)
+    }
 }
