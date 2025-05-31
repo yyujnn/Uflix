6c701c6abc9d38f5961c8d422a75c41ee1618d99
정유진 <yujin3504@gmail.com>
2025-06-01 00:32:08 +0900

    feat: MovieService 적용

--- Uflix/ViewModel/SearchResultViewModel.swift
+++ Uflix/ViewModel/SearchResultViewModel.swift
@@ -9,29 +9,16 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-enum TMDBEndpoint {
-    static func searchMovie(query: String) -> URL? {
-        let encoded = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
-        return URL(string: "https://api.themoviedb.org/3/search/movie?api_key=\(APIKeys.tmdb)&query=\(encoded)")
-    }
-}
-
 class SearchResultViewModel {
-    private let query: String
     let results = BehaviorRelay<[Movie]>(value: [])
     private let disposeBag = DisposeBag()
     
     init(query: String) {
-        self.query = query
-        fetchMovie()
+        fetchMovies(query: query)
     }
     
-    func fetchMovie() {
-        guard let url = TMDBEndpoint.searchMovie(query: query) else { return }
-        
-        NetworkManager.shared.fetch(url: url)
-            .asObservable()
-            .map { (response: MovieResponse) in response.results }
+    private func fetchMovies(query: String) {
+        MovieService.searchMovie(query: query)
             .observe(on: MainScheduler.instance)
             .subscribe(onNext: { [weak self] movies in
                 self?.results.accept(movies)
