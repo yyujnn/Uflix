444aa22db44537018e56c7822c1d14f4c98a72c4
정유진 <yujin3504@gmail.com>
2025-07-04 10:47:22 +0900

    feat: DetailViewModel에 추천 영화 로직 추가 및 로그 출력

--- Uflix/Common/MovieService.swift
+++ Uflix/Common/MovieService.swift
@@ -13,6 +13,10 @@ enum TMDBEndpoint {
         let encoded = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
         return URL(string: "https://api.themoviedb.org/3/search/movie?api_key=\(APIKeys.tmdb)&query=\(encoded)")
     }
+    
+    static func movieRecommendations(movieId: Int) -> URL? {
+            return URL(string: "https://api.themoviedb.org/3/movie/\(movieId)/recommendations?api_key=\(APIKeys.tmdb)")
+        }
 }
 
 class MovieService {
@@ -25,5 +29,15 @@ class MovieService {
         return NetworkManager.shared.fetch(url: url)
             .asObservable()
             .map{ (response: MovieResponse) in response.results }
+    }
+    
+    static func fetchRecommendations(for movieId: Int) -> Observable<[Movie]> {
+        guard let url = TMDBEndpoint.movieRecommendations(movieId: movieId) else {
+            return .error(NetworkError.invalidUrl)
+        }
+        
+        return NetworkManager.shared.fetch(url: url)
+            .asObservable()
+            .map { (response: MovieResponse) in response.results }
     }
 }
